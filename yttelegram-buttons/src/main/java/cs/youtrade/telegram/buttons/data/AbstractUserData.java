3064ccd8eb74030f04c837a7f709e443fa3e3e2c
SeeMemes <provotorov-02@mail.ru>
2026-03-20 02:54:21 +0300

    v1.0.12
    equals() and hashCode() override

--- yttelegram-buttons/src/main/java/cs/youtrade/telegram/buttons/data/AbstractUserData.java
+++ yttelegram-buttons/src/main/java/cs/youtrade/telegram/buttons/data/AbstractUserData.java
@@ -3,8 +3,22 @@ package cs.youtrade.telegram.buttons.data;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 @AllArgsConstructor
 public abstract class AbstractUserData {
     private final Long chatId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractUserData that = (AbstractUserData) o;
+        return Objects.equals(chatId, that.chatId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(chatId);
+    }
 }
